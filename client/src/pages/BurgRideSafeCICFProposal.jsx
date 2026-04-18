abe88bcb7d9365e5915b81c04f5e5a3d295213fe
kandakatlasatwik <sathwikkarthik111@gmail.com>
2026-04-18 18:10:49 +0530

    feature/burgridesafe/cicf edit

--- client/src/pages/BurgRideSafeCICFProposal.jsx
+++ client/src/pages/BurgRideSafeCICFProposal.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import cicfProposalHtml from '../../burg-ridesafe-cicf-proposal.html?raw';
 
 const BurgRideSafeCICFProposal = () => {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="/burg-ridesafe-cicf-proposal.html"
+        srcDoc={cicfProposalHtml}
         title="BURG RideSafe x Christ University - CICF Proposal"
         className="w-full h-full border-0"
-        allowFullScreen
+        sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox"
       />
     </div>
   );
