a80dc33d24e5008cce42af4186635317f885060b
SHARANYA-216 <sharanyasrinivas216@gmail.com>
2026-05-05 14:38:59 +0530

    serverAccountKey

--- server/firebase/config/firebase.js
+++ server/firebase/config/firebase.js
@@ -1,11 +1,16 @@
 // config/firebase.js
 
 const admin          = require('firebase-admin')
-const serviceAccount = require('../serviceAccountKey.json')
 const dotenv         = require('dotenv')
 const path           = require('path')
 
 dotenv.config({ path: path.join(__dirname, '..', '..', '.env') })
+
+// Load service account from ENV
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+
+// Fix private key issue
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n')
 
 // Initialize Firebase app (only once)
 if (!admin.apps.length) {
